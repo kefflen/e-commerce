d4b0e73d028a32f2675ccd93036b28df5c07c75c
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-27 11:39:30 -0300

    Criando nova variavel de ambiente MONGO_DB_URI

--- backend/src/config/dbConnect.ts
+++ backend/src/config/dbConnect.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
+const MONGO_DB_URI = process.env.MONGO_DB_URI || 'mongodb://localhost:27017/ecommerce'
 mongoose.set('strictQuery', false)
 export const dbConnect = async () => {
-  await mongoose.connect('mongodb://localhost:27017/ecommerce')
+  await mongoose.connect(MONGO_DB_URI)
 }
