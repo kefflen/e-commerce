f16e6b04996da2b625a597a3fd0c5aaaac30698c
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-29 09:43:39 -0300

    Adicioanando depedencia jsonwebtoken. Implementado ISessionManager

--- backend/src/infra/utils/SessionManager.ts
+++ backend/src/infra/utils/SessionManager.ts
@@ -1,10 +1,13 @@
+import jwt from 'jsonwebtoken'
 import {
   ISessionManager,
   userPayload,
 } from '../../domain/ports/ISessionManager'
 
 export class SessionManager implements ISessionManager {
-  createSession(payload: userPayload): Promise<string> {
-    throw new Error('Method not implemented.')
+  createSession(payload: userPayload): string {
+    return jwt.sign(payload, process.env.JWT_SECRET||'', {
+      expiresIn: '1d',
+    })
   }
 }
