58460cf807ae287fac8a49551f351bf80a793343
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-30 13:19:23 -0300

    Concertando service VerifyAuthtokenService. Retirando prefixo Bearer

--- backend/src/domain/services/auth-service/VerifyAuthTokenService.ts
+++ backend/src/domain/services/auth-service/VerifyAuthTokenService.ts
@@ -3,9 +3,13 @@ import { userPayload } from '../../ports'
 import { AuthService } from '../_contracts/AuthService'
 
 export class VerifyAuthTokenService extends AuthService {
-  async execute(token: string): Promise<userPayload> {
-    if (!token.startsWith('Bearer '))
+  async execute(bearerToken: string): Promise<userPayload> {
+    if (!bearerToken.startsWith('Bearer '))
       throw AppError.unauthorized('Invalid token')
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [_, token] = bearerToken.split(' ')
+
     const payload = this.sessionManager.verifySession(token)
 
     if (!payload) throw AppError.unauthorized('Invalid token')
