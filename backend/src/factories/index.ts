3d36e4faaf09f10dda6b64bc400362c3487e533e
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-30 13:50:56 -0300

    Criando middleware de autenticação

--- backend/src/factories/index.ts
+++ backend/src/factories/index.ts
@@ -1,1 +1,2 @@
 export * from './userServicesFactory'
+export * from './authServicesFactory'
---
+++ backend/src/routes/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../../domain/errors/AppError'
+import { authServicesFactory } from '../../factories/authServicesFactory'
+
+const {
+  verifyAuthTokenService
+} = authServicesFactory()
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const bearerToken = request.headers.authorization
+  if (!bearerToken) throw AppError.unauthorized('Need to authenticate')
+  const payload = await verifyAuthTokenService.execute(bearerToken)
+
+  request.userId = payload.id
+  next()
+}
