909b3ee09adff7e643693f23b96a4fd15aa49205
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-30 17:04:43 -0300

    Criando rota para bloquer usuario e adicionando middlewares criados nas rotas

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,18 +1,28 @@
 import { Router } from 'express'
 import {
+  blockUser,
   createUser,
   deleteUser,
   getUserById,
   listUsers,
   updatedUser,
 } from '../controllers/userController'
+import { ROLES } from '../domain/entities/User'
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
+import { ensureRole } from './middlewares/ensureRole'
 
 const userRoutes = Router()
 
 userRoutes.post('/', createUser)
-userRoutes.post('/:userId', updatedUser)
-userRoutes.get('/:userId', getUserById)
-userRoutes.delete('/:userId', deleteUser)
-userRoutes.get('/', listUsers)
+userRoutes.post('/:userId', ensureAuthenticated, updatedUser)
+userRoutes.get('/:userId', ensureAuthenticated, getUserById)
+userRoutes.delete('/:userId', ensureAuthenticated, deleteUser)
+userRoutes.get('/', ensureAuthenticated, ensureRole(ROLES.ADMIN), listUsers)
+userRoutes.put(
+  '/:userId',
+  ensureAuthenticated,
+  ensureRole(ROLES.ADMIN),
+  blockUser,
+)
 
 export default userRoutes
