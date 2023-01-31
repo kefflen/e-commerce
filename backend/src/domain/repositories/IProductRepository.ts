79cf92bd01af5a669113001d3eea4956a22600b1
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-31 16:08:40 -0300

    Alterando retorno dos metodos IProductRepository

--- backend/src/domain/repositories/IProductRepository.ts
+++ backend/src/domain/repositories/IProductRepository.ts
@@ -3,8 +3,8 @@ import { Product } from '../entities/Product'
 export interface IProductRepository {
   getProducts(): Promise<Array<Product>>
   createProduct(product: Product): Promise<Product>
-  updateProduct(id: string, product: Product): Promise<Product>
-  deleteProduct(id: string): Promise<Product>
-  getProductById(id: string): Promise<Product>
-  getProductBySlug(slug: string): Promise<Product>
+  updateProduct(id: string, product: Product|null): Promise<Product>
+  deleteProduct(id: string): Promise<void>
+  getProductById(id: string): Promise<Product|null>
+  getProductBySlug(slug: string): Promise<Product|null>
 }
