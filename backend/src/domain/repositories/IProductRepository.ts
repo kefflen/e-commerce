f9ffa8f96cefb6ddc0a018ca2bb82d5f9647b05b
Kefflen Moreno Ramos <55823300+kefflen@users.noreply.github.com>
2023-01-31 16:11:32 -0300

    Alterando IProductRepository.updateProduct. Para que não receba id e possa retornar null

--- backend/src/domain/repositories/IProductRepository.ts
+++ backend/src/domain/repositories/IProductRepository.ts
@@ -3,7 +3,7 @@ import { Product } from '../entities/Product'
 export interface IProductRepository {
   getProducts(): Promise<Array<Product>>
   createProduct(product: Product): Promise<Product>
-  updateProduct(id: string, product: Product|null): Promise<Product>
+  updateProduct(product: Product|null): Promise<Product|null>
   deleteProduct(id: string): Promise<void>
   getProductById(id: string): Promise<Product|null>
   getProductBySlug(slug: string): Promise<Product|null>
